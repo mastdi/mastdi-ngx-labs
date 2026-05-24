15b6936bbd5a6b5ccddac761d549b0dc889ade1b
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2026-05-24 21:12:27 +0200

    chore: Generate shared core library

---
+++ projects/shared-core/src/lib/shared-core.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-core',
+  imports: [],
+  template: ` <p>shared-core works!</p> `,
+  styles: ``,
+})
+export class SharedCore {}
