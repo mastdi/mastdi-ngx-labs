5b1a0e21bf9d5ed2c688a8c70552a1e34605a521
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2026-06-14 21:38:18 +0200

    feat: Decrypt header from user config

--- projects/tanita/src/app/components/vault-unlock/vault-unlock.ts
+++ projects/tanita/src/app/components/vault-unlock/vault-unlock.ts
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { UserConfig } from '../../../../../shared-core/src/lib/services/user-config';
+import { UserConfig, SecureStorage } from 'shared-core';
 
 @Component({
   selector: 'app-vault-unlock',
@@ -25,7 +25,8 @@ import { UserConfig } from '../../../../../shared-core/src/lib/services/user-con
 })
 export class VaultUnlock {
   private fb = inject(FormBuilder);
-  private userConfig = inject(UserConfig);
+  private readonly userConfig = inject(UserConfig);
+  private readonly secureStorage = inject(SecureStorage);
 
   unlockComplete = output<void>();
   storageReset = output<void>();
@@ -33,12 +34,41 @@ export class VaultUnlock {
 
   constructor() {
     this.unlockForm = this.fb.group({
-      password: ['', [Validators.required]],
+      masterPassword: ['', [Validators.required]],
     });
   }
 
-  onUnlock(): void {
+  ngOnInit() {
+    if (!this.userConfig.isConfigSet()) {
+      this.onWipeStorage();
+    }
+  }
+
+  async onUnlock(): Promise<void> {
     if (this.unlockForm.invalid) return;
+    if (!navigator.serviceWorker || !navigator.serviceWorker.controller) {
+      console.warn('Service Worker is not ready or active yet.');
+      return;
+    }
+    const { masterPassword } = this.unlockForm.value;
+    const url = this.userConfig.url;
+    let header;
+    try {
+      header = JSON.parse(
+        await this.secureStorage.decryptSecret(this.userConfig.header, masterPassword),
+      );
+    } catch {
+      // TODO: Maybe add some message as well?
+      this.unlockForm.reset();
+      return;
+    }
+    // Send the payload straight into the Service Worker thread
+    navigator.serviceWorker.controller.postMessage({
+      type: 'SET_TOKEN_CONFIG',
+      url,
+      apiTokenKey: header.key,
+      apiTokenValue: header.value,
+    });
     this.unlockComplete.emit();
   }
 
